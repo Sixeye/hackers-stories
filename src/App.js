451dbe33c6ed5267b8f8559f38fa975ce24c1192
Sixeye <srinath@srinath.fr>
2020-11-29 16:53:12 +0100

    React Props

--- src/App.js
+++ src/App.js
@@ -5,26 +5,26 @@ function getTitle(title) {
     return title
 }
 
-const list = [
-    {
-        title: 'React',
-        url: 'https://reactjs.org/',
-        author: 'Jordan Walke',
-        num_comments: 3,
-        points: 4,
-        objectID: 0,
-    },
-    {
-        title: 'Redux',
-        url: 'https://redux.js.org/',
-        author: 'Dan Abramov, Andrew Clark',
-        num_comments: 2,
-        points: 5,
-        objectID: 1,
-    },
-];
-
 const App = () => {
+
+    const stories = [
+        {
+            title: 'React',
+            url: 'https://reactjs.org/',
+            author: 'Jordan Walke',
+            num_comments: 3,
+            points: 4,
+            objectID: 0,
+        },
+        {
+            title: 'Redux',
+            url: 'https://redux.js.org/',
+            author: 'Dan Abramov, Andrew Clark',
+            num_comments: 2,
+            points: 5,
+            objectID: 1,
+        },
+    ];
 
     const handleChange = event => {
         console.log(event.target.value);
@@ -39,17 +39,17 @@ const App = () => {
 
             <hr />
 
-            <List />
+            <List list={stories}/>
         </div>
     );
 }
 
-const List = () =>
-    list.map(item => (
+const List = props =>
+    props.list.map(item => (
         <div key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
+      <span>
+        <a href={item.url}>{item.title}</a>
+      </span>
             <span>{item.author}</span>
             <span>{item.num_comments}</span>
             <span>{item.points}</span>
