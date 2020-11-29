c045c4360ccd604c03337092eab068f6bcc3a29f
Sixeye <srinath@srinath.fr>
2020-11-29 16:10:58 +0100

    another component

--- src/App.js
+++ src/App.js
@@ -5,16 +5,56 @@ function getTitle(title) {
     return title
 }
 
-function App() {
-    const greet = 'Hi'
+const list = [
+    {
+        title: 'React',
+        url: 'https://reactjs.org/',
+        author: 'Jordan Walke',
+        num_comments: 3,
+        points: 4,
+        objectID: 0,
+    },
+    {
+        title: 'Redux',
+        url: 'https://redux.js.org/',
+        author: 'Dan Abramov, Andrew Clark',
+        num_comments: 2,
+        points: 5,
+        objectID: 1,
+    },
+];
 
-  return (
-    <div>
-      <h1> 🚀 {greet} {getTitle('React JS')} </h1>
-        <label htmlFor="search">Search: </label>
-        <input id="search" type="text" />
-    </div>
-  )
+function App() {
+    const greet = 'Hallo hallo'
+
+    return (
+        <div>
+            <h1> 🚀 {greet} {getTitle('React JS')} </h1>
+            <label htmlFor="search">Search: </label>
+            <input id="search" type="text" />
+            <hr/>
+            <List />
+        </div>
+)
+}
+
+function List() {
+    return (
+        <div>
+            {list.map(function(item) {
+                return (
+                    <div key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+                        <span>{item.author}</span>
+                        <span>{item.num_comments}</span>
+                        <span>{item.points}</span>
+                    </div>
+                );
+            })}
+        </div>
+    );
 }
 
 export default App
